9c167ddd1809799fdddfa1e92a00ea3b880bd2d4
maxiaolin <maxiaolin3366@gmail.com>
2019-08-10 23:30:41 +0800

    完善日志输出

--- src/main/java/top/maxiaolin/demo/controller/HelloController.java
+++ src/main/java/top/maxiaolin/demo/controller/HelloController.java
@@ -1,5 +1,7 @@
 package top.maxiaolin.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import top.maxiaolin.demo.service.IUserInfoService;
@@ -9,10 +11,12 @@ import javax.annotation.Resource;
 @RestController
 public class HelloController {
 
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
     @Resource
     private IUserInfoService userInfoService;
     @RequestMapping("/hello")
     public Object hello(String phone){
+        logger.debug("访问到该接口----");
         return userInfoService.getOne(phone);
     }
 }
